386d12787a9c82526c0bd6e393725a65f1d549f0
Renato Gualberto <gprenatofreitas@gmail.com>
2024-04-25 19:54:10 -0300

    criado novo projeto-estrutura de uso

---
+++ DIO/Aulas-codigo/dominando-linguagem-java/src/dio/aprendendosintaxe/anatomiaclasses/BoletimEstudantil.java
@@ -0,0 +1,16 @@
+package dio.aprendendosintaxe.anatomiaclasses;
+
+public class BoletimEstudantil {
+
+    public static void main(String[] args) {
+        int mediaFinal = 7; 
+
+        if (mediaFinal < 6)
+            System.out.println("reprovado");
+        else if (mediaFinal == 6)
+            System.out.println("prova de minerva");
+        else
+            System.out.println("aprovado");
+    }
+
+}
---
+++ DIO/Aulas-codigo/dominando-linguagem-java/src/dio/aprendendosintaxe/anatomiaclasses/MinhaClasse.java
@@ -0,0 +1,34 @@
+package dio.aprendendosintaxe.anatomiaclasses;
+
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+        System.out.println("Olá mundo! Bora aprender, porque quem sabe faz a hora!");
+
+        final int ANO = 2021; // Final deixa o valor imutável ou MAIÚSCULA
+
+        // Atribuindo variável
+        String meuNome = "Renato";
+        int anoFabricacao = 2024;
+        boolean verdadeira = true;
+
+        String primeiroNome = "Carlos";
+        String segundoNome = "Fabrício";
+
+        // criado um método (operação qualquer) para retornar um valor dentro do
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        // prints
+        System.out.println(ANO);
+        System.out.println(meuNome);
+        System.out.println(anoFabricacao);
+        System.out.println(verdadeira);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+
+    }
+
+}
