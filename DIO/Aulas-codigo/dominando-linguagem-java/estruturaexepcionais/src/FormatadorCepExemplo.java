3c954c895c491d11a690111c7019aea8dcf3eee9
Renato Gualberto <gprenatofreitas@gmail.com>
2024-05-08 08:58:42 -0300

    exceções

---
+++ DIO/Aulas-codigo/dominando-linguagem-java/estruturaexepcionais/src/FormatadorCepExemplo.java
@@ -0,0 +1,18 @@
+public class FormatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String CepFormatado = formatarCep ("2376506");
+            System.out.println(CepFormatado);
+
+        } catch (CepInvalidoException e) {
+            System.out.println("O CEP não corresponde a regra de negócio.");
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+          throw new CepInvalidoException();
+        
+          //simulando um cep formatado
+          return "23.765-064";
+    }
+}
