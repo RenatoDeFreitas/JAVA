5c9db479c9c8141e5244a5e09954a7ffa985b4eb
Renato Gualberto <gprenatofreitas@gmail.com>
2024-05-08 13:06:34 -0300

    scanner-entrada-atualizado

---
+++ DIO/Aulas-codigo/dominando-linguagem-java/ValidacaoProcessoSeletivo/validarProcessoSeletivo/src/ValidacaoProcessoSeletivo.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ValidacaoProcessoSeletivo {
+    public static void main(String[] args) {
+       
+       // entrada de dados
+        Scanner entradaDados = new Scanner(System.in);
+
+        System.out.println("Informe seu nome: ");
+        String nomeCandidato = entradaDados.nextLine();
+        
+        System.out.println("Informe o salário pretendido.");
+        String salarioDesejado = entradaDados.nextLine();
+
+        // transformar em double
+        Double salarioPretendido = Double.parseDouble(salarioDesejado);
+
+       analisarCandidato(salarioPretendido);
+
+        entradaDados.close();
+    }
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println ("Ligar para o candidato. Seu salário é " + salarioPretendido);
+        }
+        else if (salarioBase == salarioPretendido){
+            System.out.println("Ligar e fazer contraproposta.");
+        }
+        else {
+            System.out.println("Aguardar resultado dos demais candidatos.");
+        }
+    }
+}
